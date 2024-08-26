7e8571ff47edf7da730626bfd5bbcea5d52431b1
rpunja321 <rpunja321>
2024-08-26 16:27:03 +0000

    change1

--- uc-setup.py
+++ uc-setup.py
@@ -1,24 +1,19 @@
 # Databricks notebook source
 # MAGIC %sql
-# MAGIC use dlt
+# MAGIC create database if not exists dlt.dev_bronze_src1 ;
+# MAGIC create database if not exists dlt.dev_silver_domain1 ;
+# MAGIC create database if not exists dlt.dev_gold
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC create database if not exists dev_bronze_src1 ;
-# MAGIC create database if not exists dev_silver_domain1 ;
-# MAGIC create database if not exists dev_gold
+# MAGIC create database if not exists dlt.qa_bronze_src1 ;
+# MAGIC create database if not exists dlt.qa_silver_domain1 ;
+# MAGIC create database if not exists dlt.qa_gold
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC create database if not exists qa_bronze_src1 ;
-# MAGIC create database if not exists qa_silver_domain1 ;
-# MAGIC create database if not exists qa_gold
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC create database if not exists prod_bronze_src1 ;
-# MAGIC create database if not exists prod_silver_domain1 ;
-# MAGIC create database if not exists prd_gold
+# MAGIC create database if not exists dlt.prod_bronze_src1 ;
+# MAGIC create database if not exists dlt.prod_silver_domain1 ;
+# MAGIC create database if not exists dlt.prd_gold
